7abf8e27c8dde5ef4fc9c2221a2bc104b51c7123
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-15 12:28:28 +0500

    form validation on all inputs applied

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 
 
@@ -14,7 +15,9 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule, SharedModule
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthModalComponent
