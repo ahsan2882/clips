a56f83802b09579c34e75abfb32f5b15e8656fc0
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-16 19:16:26 +0500

    login functionality updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import IUser from '../models/user.model';
 
 
@@ -9,19 +11,25 @@ import IUser from '../models/user.model';
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
+  public isAuthenticated$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
   ) {
     this.usersCollection = db.collection('users')
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
   }
 
   public async createUser(userData: IUser) {
     if (!userData.password) {
       throw new Error("Password not provided")
     }
-    const userCred = await this.auth.createUserWithEmailAndPassword(userData.email, userData.password)
+    const userCred = await this.auth.createUserWithEmailAndPassword(
+      userData.email, userData.password
+    )
     if (!userCred.user) {
       throw new Error("User cannot be found!")
     }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,37 @@ export class LoginComponent implements OnInit {
     email: '',
     password: ''
   }
-  constructor() { }
+
+  showAlert = false
+  alertMsg = 'Logging In....'
+  alertColor = 'blue'
+  inSubmission = false
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login() {
-    console.log(this.credentials)
+  async login() {
+    this.showAlert = true
+    this.alertMsg = 'Logging In....'
+    this.alertColor = 'blue'
+    this.inSubmission = true
+    await this.auth.signInWithEmailAndPassword(
+      this.credentials.email, this.credentials.password
+    )
+      .then(() => {
+        this.alertMsg = 'Logged in'
+        this.alertColor='green'
+      })
+      .catch(err => {
+        console.log(err)
+        this.inSubmission = false
+        let error: string = err?.message;
+        error = error.substring((error.includes('Firebase') ? 10 : 0), error.indexOf('(auth'))
+        this.alertMsg = error
+        this.alertColor = 'red'
+        return
+      })
   }
 
 }
