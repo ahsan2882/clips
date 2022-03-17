0d374902921ee09e03848634bd176c0cdfb9d146
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-17 18:49:43 +0500

    drag and drop effect on upload page

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  isDragOver = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  storeFile(event: Event) {
+    this.isDragOver = false
+  }
 }
