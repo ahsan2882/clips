5837ceccce7b0fcf8d92d669e39c60c7da76e303
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-18 16:07:11 +0500

    edit and delete clips

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { IClip } from 'src/app/models/clip.model';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
+import { IClip } from 'src/app/models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -10,34 +12,39 @@ import { ClipService } from 'src/app/services/clip.service';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1'
-  // 1 ~> ascending
-  // 2 ~> descending
-
   clips: IClip[] = []
+  activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
-  ) { }
+    private clipService: ClipService,
+    private modal: ModalService
+  ) {
+    this.sort$ = new BehaviorSubject(this.videoOrder)
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
-      this.videoOrder = (params.sort === '2') ? params.sort : '1'
+      this.videoOrder = params.sort == '2' ? params.sort : '1'
+      this.sort$.next(this.videoOrder)
     })
-    this.clipService.getUserClips().subscribe(docs => {
+    this.clipService.getUserClips(this.sort$).subscribe(docs => {
       this.clips = []
+
       docs.forEach(doc => {
         this.clips.push({
-          docID: doc.id as string,
+          docID: doc.id,
           ...doc.data()
         })
       })
     })
   }
 
-  sort($event: Event) {
-    const { value } = ($event.target as HTMLSelectElement)
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
@@ -45,4 +52,33 @@ export class ManageComponent implements OnInit {
       }
     })
   }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.activeClip = clip
+
+    this.modal.toggleModal('editClip')
+  }
+
+  update($event: IClip) {
+    this.clips.forEach((element, index) => {
+      if (element.docID == $event.docID) {
+        this.clips[index].title = $event.title
+      }
+    })
+  }
+
+  deleteClip($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.clipService.deleteClip(clip)
+
+    this.clips.forEach((element, index) => {
+      if (element.docID == clip.docID) {
+        this.clips.splice(index, 1)
+      }
+    })
+  }
+
 }
