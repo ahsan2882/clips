e010b34c0b524d04f12d13652b5173994e8f75cd
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-20 21:59:13 +0500

    lazy loading video module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,13 @@ const routes: Routes = [
     }
   },
   {
+    path: '',
+    loadChildren: async () => {
+      (await import('./video/video.module')).VideoModule
+    }
+
+  },
+  {
     path: '**',
     component: NotFoundComponent
   }
